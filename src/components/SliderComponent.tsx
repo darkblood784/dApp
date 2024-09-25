fa523bad4d04c1aa426c70e5c74860aea0d78e99
darkblood784 <a.rouniyar144@gmail.com>
2024-09-25 15:34:11 +0800

    changed the staking page

--- src/components/SliderComponent.tsx
+++ src/components/SliderComponent.tsx
@@ -57,7 +57,7 @@ const WhaleSlider: React.FC<WhaleSliderProps> = ({ sliderValue, setSliderValue, 
                 
                 {/* Whale Body: Stretches based on sliderValue */}
                 <div 
-                    className="absolute bottom-0 h-[33.5px]" 
+                    className="absolute bottom-0 h-[34px]" 
                     style={{
                         left: '-35px',  // Start the body after tail (adjust this to fine-tune alignment)
                         width: `calc(${sliderValue}% - 0px)`,  // Stretch with slider
